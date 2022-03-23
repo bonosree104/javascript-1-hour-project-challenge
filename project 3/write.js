6033c77def6f532c74cd483599edea0b763c1815
unknown <bonosreeela>
2022-03-23 13:06:23 +0600

    my complete third project

---
+++ project 3/write.js
@@ -0,0 +1,12 @@
+const text = 'this is my auto writing project dear javascript';
+
+let index =0;
+
+function writeText(){
+    document.body.innerText = text.slice(0, index);
+    index++;
+    if (index>text.length -1){
+        index =0;
+    }
+}
+setInterval(writeText,100);
